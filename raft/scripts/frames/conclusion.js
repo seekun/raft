a0585d2e12d7de9189748820e350cc79aa4c7c6d
zhouli <343787567@qq.com>
2018-04-21 11:20:59 +0800

    css

--- raft/scripts/frames/conclusion.js
+++ raft/scripts/frames/conclusion.js
@@ -24,9 +24,13 @@ define([], function () {
             })
 
             .after(500, function () {
-                frame.model().title = '<h2 style="visibility:visible">For more information:</h2>'
+                frame.model().title =
+                    '<h2 style="visibility:visible">For more information:</h2>'
+                    +'<h2 style="visibility:visible">了解更多:</h2>'
                     + '<h3 style="visibility:visible"><a href="https://ramcloud.stanford.edu/wiki/download/attachments/11370504/raft.pdf">The Raft Paper</a></h3>'
-                    + '<h3 style="visibility:visible"><a href="http://raftconsensus.github.io/">Raft Web Site</a></h3>'
+                    + '<h3 style="visibility:visible"><a href="https://ramcloud.stanford.edu/wiki/download/attachments/11370504/raft.pdf">Raft论文（PDF）</a></h3>'
+                    + '<h3 style="visibility:visible"><a href="https://raft.github.io/">Raft Web Site</a></h3>'
+                    + '<h3 style="visibility:visible"><a href="https://raft.github.io/">Raft 一致性算法</a></h3>'
                     + '<br/>' + frame.model().controls.html();
                 layout.invalidate();
             })
