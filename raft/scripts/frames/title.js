6cdbe206ec114ae6cdd8530ffa6da0d5dac8f58c
zhouli <343787567@qq.com>
2018-04-21 14:51:52 +0800

    1.0

--- raft/scripts/frames/title.js
+++ raft/scripts/frames/title.js
@@ -13,15 +13,14 @@ define([], function () {
         })
 
             .after(500, function () {
-                frame.model().title = '<h3 style="visibility:visible">Raft</h3>'
-                    + '<h4 style="visibility:visible">Understandable Distributed Consensus</h4>'
-                    + '<h4 style="visibility:visible">轻松理解分布式一致性算法——Raft</h4>'
+                frame.model().title = '<h4 style="visibility:visible">Raft</h4>'
+                    + '<h5 style="visibility:visible">Understandable Distributed Consensus</h5>'
+                    + '<h5 style="visibility:visible">轻松理解分布式一致性算法——Raft</h5>'
                     + '<br/>' + frame.model().controls.html();
                 layout.invalidate();
             })
             .after(500, function () {
                 frame.model().subtitle = '<p style="visibility:visible">' +
-                    '<a href="https://github.com/djmpink/raft" target="_blank">Github</a> | ' +
                     '<a href="http://7player.cn/" target="_blank">7player.cn</a> | ' +
                     '<a href="http://7player.cn/" target="_blank">中文</a> | ' +
                     '<a href="http://thesecretlivesofdata.com/raft/" target="_blank">英文原版</a> ' +
